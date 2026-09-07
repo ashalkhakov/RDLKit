79b706f1ec3a3857ca7a748427346b7814c93c7e
Artyom Shalkhakov <artyom.shalkhakov@gmail.com>
2026-09-07 10:27:08 +0500

    Remove temp files.

--- Scripts/gnustep-box/dumpxml.m
+++
@@ -1,6 +0,0 @@
-#import <Foundation/Foundation.h>
-#import "RDLKit.h"
-int main(void) { @autoreleasepool {
-  RDLReport *r = [RDLReport emptyReportNamed:@"Empty"];
-  printf("%s\n", [[RDLWriter XMLStringFromReport:r] UTF8String]);
-} return 0; }
